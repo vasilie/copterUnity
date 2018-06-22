abd4650b0b65f8cf391635442829b7ea16ea9aa2
_vasilie <vasilije.milenkovic@diwanee.com>
2018-06-22 16:56:10 +0200

    ingroe

---
+++ Assets/Copter.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Copter : MonoBehaviour {
+
+    public float maxAngVel = 100.0f;
+    public float angDrag = 1.0f;
+    public float spinForce = 100.0f;
+    public float turnForce = 10.0f;
+    public float nudgeForce = 2.0f;
+
+
+    private float hudY = -50f;
+
+    private Transform rocketHolder1;
+    private Transform rocketHolder2;
+
+    public GameObject rocket;
+
+    //public Transform propeler;
+    //public Component[] rigidbodys;
+    public Rigidbody propelerBody;
+    public GameObject propeler;
+    public float upForce = 1.0f;
+
+    private RectTransform hud_pink;
+ 	// Use this for initialization
+	void Start () {
+        rocketHolder1 = transform.Find("rocketHolder1");
+        rocketHolder2 = transform.Find("rocketHolder2");
+
+        hud_pink = GameObject.Find("hud_pink").GetComponent<RectTransform>();
+
+     
+
+        propelerBody = transform.Find("propelers").GetComponent<Rigidbody>();
+        //foreach (Rigidbody body in rigidbodys){
+        //    if (body.GetComponentInParent<Transform>().name == "propelers"){
+        //        Debug.Log("found");
+        //        propelerBody = body;
+        //    }
+        //}
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        propelerBody.angularDrag = angDrag;
+        propelerBody.maxAngularVelocity = maxAngVel;
+
+        if (Input.GetKey("space"))
+        {
+            if (propelerBody.angularVelocity.y >= 10.0f){
+               
+
+                gameObject.GetComponent<Rigidbody>().AddForce(transform.up * upForce);
+            }
+
+           
+            propelerBody.AddTorque(transform.up * spinForce);
+        }
+        if (Input.GetKey("e")){
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * turnForce);
+        }
+        if (Input.GetKey("q"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * -turnForce);
+        }
+        if (Input.GetKey("w"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * nudgeForce);
+          
+        }
+        if (Input.GetKey("s"))
+        {
+          
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * -nudgeForce);
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.forward * nudgeForce);
+        }
+        if (Input.GetKey("d"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddTorque(transform.forward * -nudgeForce);
+        }
+        if (Input.GetKeyDown("u"))
+        {
+            Instantiate(rocket, rocketHolder1.position, rocketHolder1.rotation);
+            Instantiate(rocket, rocketHolder2.position, rocketHolder1.rotation);
+        }
+        if (transform.eulerAngles.x > 90){
+            hudY = (0.55f * transform.eulerAngles.x % 270f) - 250f;
+
+        } else {
+            hudY = (0.55f * transform.eulerAngles.x % 270f) -50f;
+        }
+        Debug.Log(hudY);
+
+        //hudY = 0.55f * (transform.eulerAngles.x +90f);
+        //Debug.Log(transform.eulerAngles.x % 9);
+        //Debug.Log(hudY);
+
+        //hud_pink.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z);
+        hud_pink.transform.position = new Vector3(0f, hudY, 0f);
+	}
+}
---
+++ Assets/rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+    public GameObject explosionEffect;
+    public float rocketSpeed = 10.0f;
+    public float explosionForce = 10.0f;
+    public float explosionRadius = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("hit lf");
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        foreach (Collider nearbyObject in colliders){
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null){
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
+        Destroy(gameObject);
+
+    }
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * rocketSpeed);
+	}
+}
